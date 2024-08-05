ecb8761adff5ca9e0e075277835374efb4679f28
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-04 18:57:56 -0700

    Display button clicks

--- script.js
+++ script.js
@@ -24,3 +24,22 @@ function operate(num1, operator, num2){
     if(operator === '*') return multiply(num1, num2);
     if(operator === '/') return divide(num1, num2);
 }
+
+let displayValue = [];
+const display = document.querySelector('.display');
+display.textContent = 0;
+
+function populateDisplay(num) {
+    displayValue.push(num);
+    let text = displayValue.join('');
+    display.textContent = text;
+    // num = Number(num)
+    // if(isNaN(num)) return;
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        populateDisplay(button.textContent);
+    });
+});
