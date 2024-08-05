1522d457a84c2f2cc03494cf2d9ae83f940d48b2
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-04 18:19:37 -0700

    Add buttons and math functions

---
+++ script.js
@@ -0,0 +1,26 @@
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(num1, operator, num2){
+    if(operator === '+') return add(num1, num2);
+    if(operator === '-') return subtract(num1, num2);
+    if(operator === '*') return multiply(num1, num2);
+    if(operator === '/') return divide(num1, num2);
+}
